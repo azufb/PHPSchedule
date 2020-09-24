8b55e8a775b13010829e1e3e3b0b60ac051d2cf2
Azusa Okamoto <curiousazunas@gmail.com>
2020-09-24 20:16:37 +0900

    カレンダーの日付表示

---
+++ schedule/app/Calendar/CalendarWeekBlankDay.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Calendar;
+
+class CalendarWeekBlankDay extends CalendarWeekDay {
+    function getClassName() {
+        return "day-blank";
+    }
+
+    /**
+     * @return
+     */
+
+     function render() {
+        return '';
+     }
+}
+?>
